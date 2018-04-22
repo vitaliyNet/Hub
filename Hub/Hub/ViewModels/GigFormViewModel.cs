023ceb225c12633285dc62da4a4868d1382bc396
vitaliyNet <kmda.mail@gmail.com>
2018-04-22 20:46:08 +0300

    Create Add a Gig form.

---
+++ Hub/Hub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Hub.Models;
+using System.Collections.Generic;
+
+namespace Hub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
