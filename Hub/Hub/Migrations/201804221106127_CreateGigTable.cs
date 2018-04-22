a6d5a15e3dc2889923fb03afcade5712ce68dcfe
vitaliyNet <kmda.mail@gmail.com>
2018-04-22 18:13:54 +0300

    Create the gig model

---
+++ Hub/Hub/Migrations/201804221106127_CreateGigTable.cs
@@ -0,0 +1,47 @@
+namespace Hub.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateGigTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Gigs",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        DateTime = c.DateTime(nullable: false),
+                        Venue = c.String(),
+                        Artist_Id = c.String(maxLength: 128),
+                        Gengre_Id = c.Byte(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.Artist_Id)
+                .ForeignKey("dbo.Genres", t => t.Gengre_Id)
+                .Index(t => t.Artist_Id)
+                .Index(t => t.Gengre_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Gigs", "Gengre_Id", "dbo.Genres");
+            DropForeignKey("dbo.Gigs", "Artist_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.Gigs", new[] { "Gengre_Id" });
+            DropIndex("dbo.Gigs", new[] { "Artist_Id" });
+            DropTable("dbo.Gigs");
+            DropTable("dbo.Genres");
+        }
+    }
+}
---
+++ Hub/Hub/Models/Gig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; } // who is performing
+
+        public DateTime DateTime { get; set; } //when gig is gonna happend
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; } // where is gonna happend
+
+        [Required]
+        public Genre Gengre { get; set; } // what genre is it
+    }
+}
